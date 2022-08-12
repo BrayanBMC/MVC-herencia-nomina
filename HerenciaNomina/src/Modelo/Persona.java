d8085ca82dde56e92f04f2b7489686665d6ff7d0
BrayanBMC <107967931+BrayanBMC@users.noreply.github.com>
2022-08-12 17:50:24 -0500

    Add files via upload

---
+++ HerenciaNomina/src/Modelo/Persona.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+
+public class Persona {
+    String nombre,id;
+    public String getNombre() {
+    return nombre;
+    }
+    public void setNombre(String nombre) {
+    this.nombre = nombre;
+    }
+    public String getId() {
+    return id;
+    }
+    public void setId(String id) {
+    this.id = id;
+    }
+    
+}
